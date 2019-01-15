7952e7f011e62b44a541f43a5738288cd9d67c83
NicholasCF <nicholas.cristian@yahoo.com>
2019-01-15 10:28:19 +0800

    Migrated database for TimeRange and Place

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_10_031706) do
+ActiveRecord::Schema.define(version: 2019_01_15_022332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,19 @@ ActiveRecord::Schema.define(version: 2018_11_10_031706) do
     t.string "email"
     t.string "cell"
     t.boolean "MC"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "time_ranges", force: :cascade do |t|
+    t.time "start"
+    t.time "end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
